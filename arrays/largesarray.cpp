9b10878a2dbd81b62d32f6631da424e8b058c66f
13poorvi03 <poorvisharma0306@gmail.com>
2026-09-05 15:50:17 +0530

    added

---
+++ arrays/largesarray.cpp
@@ -0,0 +1,21 @@
+
+// for largest value 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int nums[] ={5,15,22,1,-15,24};
+    int size = 6;
+    int largest =INT_MIN;
+    for(int i=0 ; i<size ; i++){
+        if(nums[i] > largest){
+            largest = nums[i];
+        }
+    }
+
+    cout<<"smallest :"<<largest <<endl;
+
+    return 0;
+}
